7081df4861bf38499f31dd350aee0c0a5df7c66e
Vladislav <64088661+alteropt@users.noreply.github.com>
2024-03-29 18:35:53 +0300

    responsive

---
+++ js/index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Navigation Responsive
+  const burger = document.getElementById('burger')
+  const navigation = document.getElementById('navigation')
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    navigation.classList.toggle('active')
+  })
+
+  document.addEventListener( 'click', (e) => {
+    const withinBoundariesNavigation = e.composedPath().includes(navigation);
+    const withinBoundariesBurger = e.composedPath().includes(burger)
+
+    if (!withinBoundariesNavigation && !withinBoundariesBurger) {
+      burger.classList.remove('active')
+      navigation.classList.remove('active')  
+    }
+  })
+})
